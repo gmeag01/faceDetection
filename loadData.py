915810500da30d5737c9fa17d0f53ed43d51cb7e
gmeag01 <skapur749@gmail.com>
2022-11-15 10:40:55 +0900

    Preprocessing and Get data with Haar Cascade Algorithm

--- loadData.py
+++ loadData.py
@@ -7,15 +7,26 @@ Then you can get a labels.csv file including name, path, min & max of x,y coordi
 '''
 
 import os
+import numpy as np
 import csv
 import cv2
 
 # Get bounding box coordinate about face with Haar Cascade algorithm
 def get_face_coordinate(path):
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)   # Convert color image to grayscale image
-    face_cascade = cv2.CascadeClassifier('./model/haarcascade_frontalface_default.xml')   # model import
-    face = face_cascade.detectMultiScale(img, 1.5, 5)   # Get coordinate
-    return face
+    img = cv2.imread(path)   # Convert color image to grayscale image
+    frontal_face_cascade = cv2.CascadeClassifier('./model/haarcascade_frontalface_default.xml')   # model import
+    profile_face_cascade = cv2.CascadeClassifier('./model/haarcascade_profileface.xml')
+    frontal_face = np.array(frontal_face_cascade.detectMultiScale(img, 1.5, 5))   # Get coordinate
+    profile_face = np.array(profile_face_cascade.detectMultiScale(img, 1.5, 5))
+
+    if frontal_face.any() and profile_face.any(): 
+        return np.concatenate([frontal_face, profile_face])
+    elif not frontal_face.any() and profile_face.any(): 
+        return profile_face
+    elif frontal_face.any() and not profile_face.any(): 
+        return frontal_face
+    else:
+        pass
 
 # Save coordinate to csv format(image_name, image_path, x_min, y_min, x_max, y_max) 
 def save_coordinate(path, filename):
@@ -37,27 +48,28 @@ def make_data(path):
         except:
             pass  # if algorithm didn't get coordinate, just skip it.
 
-def get_frame(video_folder_path, frame_name, fps):
-    n = 0
+def get_frame(video_folder_path, fps):
     for video_name in [video_path for video_path in os.listdir(video_folder_path) if video_path.endswith('.mp4')]:
+        n = 0
         cap = cv2.VideoCapture(os.path.join(video_folder_path, video_name))
 
         while(cap.isOpened()):
+            if n == 1000 : break
             ret, frame = cap.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             frame = cv2.resize(frame, (960, 960))
             if ret:
                 if(int(cap.get(1)) % fps == 0):
-                    path = os.path.join(video_folder_path, frame_name) + str(n) + '.jpg'
+                    path = os.path.join(video_folder_path, video_name[:-4])+ '_' + str(n) + '.jpg'
                     cv2.imwrite(path, frame)
                 n += 1
         cap.release()
 try:
     path = './data'
-    with open(os.path.join(path, 'labels.csv'), 'a', newline='') as file:
+    with open(os.path.join(path, 'labels.csv'), 'w', newline='') as file:
         wr = csv.writer(file)
         wr.writerow(['name', 'path', 'x_min', 'y_min', 'x_max', 'y_max'])
-    get_frame(path, 'img', 60)
+    get_frame(path, 30)
     make_data(path)
 except Exception as e:
     print('Error : ', e)
